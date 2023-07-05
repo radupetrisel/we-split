5ff05ebe38c0edd7b5185d77b67d02fa6dbd160f
Radu Petrisel <radupetrisel@gmail.com>
2023-07-05 16:38:23 +0300

    Create app based on tutorial
    * add TextField for inputting check amount
    * add Pickers for number of people and tip percentage
    * add output Text for amount to be paid by each person
    * add "Done" button to keyboard when TextField is focused

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    private static let currencyIdentifier = Locale.current.currency?.identifier ?? "RON"
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    @FocusState private var isAmountFocused: Bool
+    
+    let tipPercentages = [0, 5, 10, 15, 20, 25]
+    
+    var totalPerPerson: Double {
+        let grandTotal = checkAmount * (1 + Double(tipPercentage) / 100)
+        return grandTotal / Double(numberOfPeople)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Self.currencyIdentifier))
+                        .keyboardType(.decimalPad)
+                        .focused($isAmountFocused)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100, id: \.self) { Text("\($0) people") }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) { Text($0, format: .percent) }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Self.currencyIdentifier))
+                } header: {
+                    Text("Each person must pay")
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        isAmountFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
